d92e0d32d5c35cea1b259d27aaddbe0f97d99e65
caolibin <1265501579@qq.com>
2024-03-30 18:20:25 +0800

    合并dev分支

---
+++ feign-client/src/main/java/io/github/tankingcao/FeignClientApplication.java
@@ -0,0 +1,13 @@
+package io.github.tankingcao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeignClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeignClientApplication.class, args);
+    }
+
+}
